3ba0708525a464d7622e58a0d95238c364cc218c
DELL <fadymonier111@gmail.com>
2024-11-07 18:01:59 +0200

    appColors

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesDalelLogo
+  /// assets/images/dalelLogo.png
+  static const String imagesDalelLogo = "assets/images/dalelLogo.png";
+
+  /// Assets for imagesForgotPassword
+  /// assets/images/forgotPassword.png
+  static const String imagesForgotPassword = "assets/images/forgotPassword.png";
+
+  /// Assets for imagesOnboarding1
+  /// assets/images/onboarding1.png
+  static const String imagesOnboarding1 = "assets/images/onboarding1.png";
+
+  /// Assets for imagesOnboarding2
+  /// assets/images/onboarding2.png
+  static const String imagesOnboarding2 = "assets/images/onboarding2.png";
+
+  /// Assets for imagesOnboarding3
+  /// assets/images/onboarding3.png
+  static const String imagesOnboarding3 = "assets/images/onboarding3.png";
+
+  /// Assets for imagesOrderSuccess
+  /// assets/images/orderSuccess.png
+  static const String imagesOrderSuccess = "assets/images/orderSuccess.png";
+}
+
