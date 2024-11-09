f7f2f2d23f284973b337b23c7d5424029545655f
DELL <fadymonier111@gmail.com>
2024-11-09 19:53:58 +0200

    app_router

---
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SplashSCreen extends StatelessWidget {
+  const SplashSCreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
